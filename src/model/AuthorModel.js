3d6a61d52b003bf2aac61fe6bd76202047b49f16
Sreeja0202 <106142975+Sreeja0202@users.noreply.github.com>
2022-08-17 16:37:39 +0530

    updated

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 // const url = "mongodb+srv://sreejamohan444:cvn8292R5w6QVsEw@cluster0.mwlsecd.mongodb.net/LibraryApp";
 // mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+// 
 // mongoose.connect('mongodb+srv://sreejamohan444:Devsree0202@cluster0.mwlsecd.mongodb.net/LibraryApp');
 
-mongoose.connect("mongodb+srv://sreejamohan444:q7tlWx5Aa8mAi2LS@cluster0.mwlsecd.mongodb.net/LibraryApp?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://sreejamohan444:q7tlWx5Aa8mAi2LS@cluster0.mwlsecd.mongodb.net/LibraryApp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Schema = mongoose.Schema;
 
